297f186f0967562ba00f5629862caf17e236d093
Marc OLIVIERI <maolivie@student.42.fr>
2019-01-06 18:54:48 +0100

    Verifie mtn quil n'y a pas plus de 26 treti

--- check.c
+++ check.c
@@ -6,7 +6,7 @@
 /*   By: maolivie <maolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 15:37:18 by kemethen          #+#    #+#             */
-/*   Updated: 2019/01/06 18:30:45 by maolivie         ###   ########.fr       */
+/*   Updated: 2019/01/06 18:54:07 by maolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,13 +56,15 @@ static short	check_tetri(char *buff)
 short			check_file(int fd)
 {
 	char	buff[21];
+	short	tetri;
 	short	ret;
 	short	last_ret;
 
+	tetri = 0;
 	last_ret = 0;
 	while ((ret = read(fd, buff, 21)) > 0)
 	{
-		if (ret < 20)
+		if (ret < 20 || tetri == 26)
 			return (0);
 		if (!check_format(buff))
 			return (0);
@@ -70,6 +72,7 @@ short			check_file(int fd)
 			return (0);
 		if (ret == 21 && buff[20] != '\n')
 			return (0);
+		tetri++;
 		last_ret = ret;
 	}
 	return (last_ret == 20);
