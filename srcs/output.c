eb130f9418ec6430fb08583fdd3ac86c887c2a0f
Marc OLIVIERI <maolivie@student.42.fr>
2019-01-07 17:39:36 +0100

    return at end of block

--- srcs/output.c
+++ srcs/output.c
@@ -6,7 +6,7 @@
 /*   By: maolivie <maolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 14:30:52 by maolivie          #+#    #+#             */
-/*   Updated: 2019/01/07 17:34:34 by maolivie         ###   ########.fr       */
+/*   Updated: 2019/01/07 17:38:44 by maolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ static void		display_a_tetri(unsigned short tetri)
 		if (i % 4 == 0)
 			write(1, "\n", 1);
 	}
+	return ;
 }
 
 void			display_all_tetris(unsigned short *parsed)
